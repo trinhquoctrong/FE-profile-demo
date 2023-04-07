6490bdc3b874b588dd928f693ff161da42ad8430
Trong Trinh <trinhquoctrong09112001@gmail.com>
2023-04-07 15:39:24 +0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function openTabProject1() {
+  window.open("https://front-end-project-mini-1-trinh-quoc-trong.vercel.app", "_blank");
+}
+
+function openTabProject2() {
+  window.open("https://front-end-project-mini-2-trinh-quoc-trong.vercel.app", "_blank");
+}
+
+function openTabProject3() {
+  window.open("https://front-end-project-mini-1-trinh-quoc-trong.vercel.app", "_blank");
+}
